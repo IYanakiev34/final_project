cc4739591a93371146f5fada360193f50f4af017
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2022-05-25 20:15:32 +0200

    usageInfo page done

--- pages/usageInfo.js
+++ pages/usageInfo.js
@@ -5,10 +5,59 @@ export default function Usage(props) {
     const data = props.data;
 
     return (
-        <div>
-            <h1>{data.service}</h1>
-            <h1>{data.usage}</h1>
-            <h1>{data.limit}</h1>
+        <div className="flex flex-col content-center">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                        <table className="min-w-full">
+                            <thead className="bg-white border-b">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        #
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        First
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        Last
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        Handle
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="bg-gray-100 border-b">
+                                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        1
+                                    </td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {data.service}
+                                    </td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {data.usage}
+                                    </td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {data.limit}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
